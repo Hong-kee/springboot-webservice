045dba73c7a769bbc9b078b6ea17b54630d5ab8b
harrykim-nsuslab <harry.kim@nsuslab.com>
2023-01-09 01:52:56 +0900

    Controllers for CRUD.

--- src/main/java/com/harry/springboot/web/IndexController.java
+++ src/main/java/com/harry/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.harry.springboot.web;
 
 import com.harry.springboot.service.posts.PostsService;
+import com.harry.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,5 +24,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id,
+                              Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
--- src/main/java/com/harry/springboot/web/PostsApiController.java
+++ src/main/java/com/harry/springboot/web/PostsApiController.java
@@ -1,11 +1,15 @@
 package com.harry.springboot.web;
 
+import com.harry.springboot.domain.posts.Posts;
 import com.harry.springboot.service.posts.PostsService;
+import com.harry.springboot.web.dto.PostsListResponseDto;
 import com.harry.springboot.web.dto.PostsResponseDto;
 import com.harry.springboot.web.dto.PostsSaveRequestDto;
 import com.harry.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +26,18 @@ public class PostsApiController {
     public Long update(@PathVariable Long id,
                        @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 
     @GetMapping("/api/v1/posts/{id}")
