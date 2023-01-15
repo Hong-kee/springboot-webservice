b45338f7c22a805122cfbc94b71b03fafcafa853
harrykim-nsuslab <harry.kim@nsuslab.com>
2023-01-15 22:08:46 +0900

    Dto and service setting for OAuth2.

---
+++ src/main/java/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package config.auth.dto;
+
+import com.harry.springboot.domain.user.User;
+
+import java.io.Serializable;
+
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+
+}
